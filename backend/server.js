c980216f1385cd7757ce975bc42f90a53a5be041
Adrian Pueblo <adrian.pueblo27@gmail.com>
2025-03-28 07:21:58 +0800

    Added middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
+const cors = require(cors)
+
 const express = require('express')
 const app = express()
 
 const PORT = 3000
-console.log('Hi')
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res) => {
+    console.log(req.path, req.method)
+    next()
+})
 
 app.listen(PORT)
