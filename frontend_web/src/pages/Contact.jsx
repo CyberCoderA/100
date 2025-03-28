16a4716a1212ae39598f24f78eab7b6fef9ac4b9
Adrian Pueblo <adrian.pueblo27@gmail.com>
2025-03-28 16:54:07 +0800

    Fiexd sup in contact.

--- frontend_web/src/pages/Contact.jsx
+++ frontend_web/src/pages/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
                 <div>
                     <h1 className="text-3xl xl:text-7xl text-dark-heavy font-bold">Adrian Pueblo</h1>
                     <p className='text-dark-heavy mb-30 w-150 mt-10 xl:text-4xl xl:w-230'>
-                    I’m a 3rd year IT student creating projects to build up my portfolio. You can contact me from the provided information below.
+                    I’m a 3<sup>rd</sup> year IT student creating projects to build up my portfolio. You can contact me from the provided information below.
                     </p>
                 </div>
             </div>
