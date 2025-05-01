5eea4c20141a8f43f22610b57345ff97ea715935
Adrian Pueblo <adrian.pueblo27@gmail.com>
2025-05-01 09:59:34 +0800

    Test 7

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -9,7 +9,7 @@ const getUser = async(req, res) => {
 
         res.status(200).json(user);
     } catch (error) {
-        res.status(500).json({message: error});
+        res.status(500).json({message: "Server Error: 1"});
     }
 }
 
