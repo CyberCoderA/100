fead015fff92cd59687dd8fd8ec855d8f217dd01
CyberCoderA <89377763+CyberCoderA@users.noreply.github.com>
2025-11-20 10:07:18 +0800

    Test 3

--- frontend_web/src/components/Navbar.jsx
+++ frontend_web/src/components/Navbar.jsx
@@ -16,11 +16,6 @@ const Navbar = ({ activeRoute }) => {
     function renderRoutes() {
         siteRoutes.map((i) => {
             console.log(i.replace('/', "").charAt(0).toUpperCase() + i.substring(2))
-            // if (i == activeRoute) {
-            //     <Link to={i}><li className='p-3 hover:bg-gray-600 hover:text-white rounded-md transition-all cursor-pointer font-bold'>{i.replace('/', "").charAt(0).toUpperCase() + i.substring(2)}!</li></Link>
-            // } else {
-            //     <Link to={i}><li className='p-3 hover:bg-gray-600 hover:text-white rounded-md transition-all cursor-pointer'>{i.replace('/', "").charAt(0).toUpperCase() + i.substring(2)}!</li></Link>
-            // }
 
             <h1>{i.replace('/', "").charAt(0).toUpperCase() + i.substring(2)}</h1>
         })
