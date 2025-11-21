b398ff16936381abf9fcea3e247e72d9bc8fc489
CyberCoderA <89377763+CyberCoderA@users.noreply.github.com>
2025-11-21 10:40:31 +0800

    Dynamic nav account

--- frontend_web/src/components/NavUserAccountLink.jsx
+++ frontend_web/src/components/NavUserAccountLink.jsx
@@ -3,11 +3,24 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 
 const NavUserAccountLink = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    function NotLoginNavActions() {
+        return (
+            <ul className='hidden xl:flex flex-row items-center gap-3 text-dark-heavy text-2xl h-20'>
+                <Link to={"/login"}><li className='p-3 hover:bg-gray-600 hover:text-white rounded-md transition-all xl:text-3xl cursor-pointer'>Login</li></Link>
+                <li className="xl:text-xl"> | </li>
+                <Link to={"/register"}><li className='p-3 hover:bg-gray-600 hover:text-white rounded-md transition-all xl:text-3xl cursor-pointer'>Register</li></Link>
+            </ul>
+        )
+    }
+
+    function handleIfUserIsLogin() {
+        return isLoggedIn ? NotLoginNavActions : <p>Account</p>;
+    }
+
     return (
-        <ul className='hidden xl:flex flex-row items-center gap-8 text-dark-heavy text-2xl h-20'>
-            <Link to={"/login"}><li className='p-3 hover:bg-gray-600 hover:text-white rounded-md transition-all xl:text-3xl cursor-pointer border-left-black'>Login</li></Link>
-            <Link to={"/register"}><li className='p-3 hover:bg-gray-600 hover:text-white rounded-md transition-all xl:text-3xl cursor-pointer'>Register</li></Link>
-        </ul>
+        {handleIfUserIsLogin()}
     )
 };
 
