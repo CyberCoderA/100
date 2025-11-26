af9c3e081ca923c49a8b3928d116419ba43d9be3
CyberCoderA <89377763+CyberCoderA@users.noreply.github.com>
2025-11-26 15:14:54 +0800

    Tweaked the login modal.

--- frontend_web/src/pages/Login.jsx
+++ frontend_web/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
             {/* Body */}
             <div className='background-img flex justify-center items-center relative h-screen overflow-hidden'>
                 {/* Login Modal */}
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between px-5 py-2 xl:px-10 xl:py-1 xl:justify-around w-70 h-100 bg-white rounded-3xl border-1 xl:w-180 xl:h-180">
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between px-5 py-2 w-70 h-100 bg-white border-1 rounded-xl xl:px-10 xl:py-1 xl:justify-around xl:w-180 xl:h-180">
                     <h1 className='text-dark-heavy self-center font-bold text-2xl xl:text-6xl'>USER LOGIN</h1>
                     <div>
                         <InputField title="Email Address" placeholder="someones@gmail.com" /> 
